1994ac527704f2d5309c45f29125ca1e5e734e06
Nabeegh <nabeegh77mumtaz@gmail.com>
2023-01-08 18:45:53 +0500

    stats api corrected

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -103,7 +103,7 @@ async function countCurrentWeekDocuments(Document) {
     ];
   
     const result = await Document.aggregate(pipeline);
-    return result[0].count;
+    return result.length > 0 ?  result[0].count : 0;
   }
   
   
